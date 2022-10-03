d54a61fe58d97b7826058f359d26728479be14d7
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-10-03 22:29:41 +0300

    Create 03. Even or Odd.js

---
+++ Programming basics/Conditional Statements/03. Even or Odd.js
@@ -0,0 +1,11 @@
+function evenOdd (input) {
+let a= Number (input[0]);
+let b= Number (input [1]);
+if (a%2==0) {
+    console.log ("even")
+}
+else {
+    console.log ("odd")
+}
+}
+evenOdd (["2", "3"])
