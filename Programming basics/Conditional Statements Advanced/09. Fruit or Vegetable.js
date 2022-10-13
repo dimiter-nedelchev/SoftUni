93f4dc9931618d95a50ec2219bab5ff2add03888
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-10-13 07:00:07 +0300

    Create 09. Fruit or Vegetable.js

---
+++ Programming basics/Conditional Statements Advanced/09. Fruit or Vegetable.js
@@ -0,0 +1,27 @@
+function fruitOrVegetable (input) {
+let product=input [0]
+
+switch (product) {
+    case "banana":
+        case "apple":
+            case "cherry":
+                case "kiwi":
+                    case "lemon":
+                        case "grapes":
+                            console.log ("fruit")
+                            break
+                            case "tomato":
+                                case "carrot":
+                                    case "cucumber":
+                                        case "pepper":
+                                            console.log ("vegetable")
+                                            break
+                                            default:
+                                                console.log ("unknown")
+                                                break
+}
+}
+//fruitOrVegetable (["banana"])
+//fruitOrVegetable (["apple"])
+//fruitOrVegetable (["tomato"])
+//fruitOrVegetable (["water"])
