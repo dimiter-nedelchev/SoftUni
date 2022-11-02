8c8fcd70df5f1b863082f77c27e67a12f68df3d8
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-11-02 06:13:56 +0200

    Create 04. Sequence 2k+1.js

---
+++ Programming basics/While Loops/04. Sequence 2k+1.js
@@ -0,0 +1,11 @@
+function sequence (input) {
+let index=0
+let num=Number (input [index])
+let number=1
+
+while (number<=num) {
+    console.log (number)
+    number=(number*2)+1
+}
+}
+sequence (["8"])
