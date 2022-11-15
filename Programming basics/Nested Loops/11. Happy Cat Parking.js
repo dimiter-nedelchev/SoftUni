5a6b8e4ac092194ae9ac8949d8d48c16d2fe15a2
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-11-15 06:54:40 +0200

    Create 11. Happy Cat Parking.js

---
+++ Programming basics/Nested Loops/11. Happy Cat Parking.js
@@ -0,0 +1,29 @@
+function parking (input) {
+
+    let days=Number (input [0])
+    let hours=Number (input [1])
+
+    let total=0
+    let endSum=0
+    let daySum=''
+
+    for (i=1;i<=days;i++) {
+        for (j=1;j<=hours;j++) {
+            if (i%2===0 && j%2!==0) {
+                total += 2.5
+            }
+            else if (i%2!==0 && j%2===0) {
+                total += 1.25
+            }
+            else {
+                total += 1
+            }
+        }
+        daySum += "Day:" + " " + i.toString () + " " + "-" + " " + total.toFixed(2) + " " + "leva" + "\n"
+        endSum += total
+        total=0
+  }
+  console.log (`${daySum}Total: ${endSum.toFixed(2)} leva`)
+}
+//parking (["2", "5"])
+parking (["5", "2"])
