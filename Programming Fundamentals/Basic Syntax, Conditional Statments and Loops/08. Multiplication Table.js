560cb17a254d62c91c185f0fd045033e5d3e2879
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-12-05 07:24:58 +0200

    Create 08. Multiplication Table.js

---
+++ Programming Fundamentals/Basic Syntax, Conditional Statments and Loops/08. Multiplication Table.js
@@ -0,0 +1,12 @@
+function multiplication(num) {
+
+    let result=0;
+
+    for (i=1;i<=10;i++) {
+        result=num*i;
+
+        console.log(`${num} X ${i} = ${result}`)
+    }
+}
+multiplication(5);
+//multiplication(2);
