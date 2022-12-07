cf93d3fe9832a5ff7a1f302c4f7ecbfc6a30897e
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-12-07 07:54:07 +0200

    Create 05. Distance Between Points.js

---
+++ Programming Fundamentals/Basic Syntax, Conditional Statments and Loops/05. Distance Between Points.js
@@ -0,0 +1,9 @@
+function distancePoints(x1,y1,x2,y2) {
+
+    distance= Math.sqrt(Math.pow(x2-x1,2) + Math.pow(y2-y1,2));
+
+    console.log((distance))
+}
+
+distancePoints(2,4,5,0);
+distancePoints(2.34,15.66,-13.55,-2.9985);
