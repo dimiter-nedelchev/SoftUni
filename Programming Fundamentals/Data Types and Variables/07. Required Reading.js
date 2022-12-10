18744f8921400bbc23746ddda79a30ac1320c5d1
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-12-10 08:43:07 +0200

    Create 07. Required Reading.js

---
+++ Programming Fundamentals/Data Types and Variables/07. Required Reading.js
@@ -0,0 +1,10 @@
+function requiredReading(bookPages,pagesPerHour,days){
+
+    let hours=bookPages/pagesPerHour;
+
+    let hoursPerDay=hours/days;
+
+    console.log(hoursPerDay)
+}
+requiredReading(212, 20 ,2 );
+requiredReading(432,15 ,4 );
