c67c056c8f07b0efb71e42b2eb9719c7abceabfb
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-12-10 08:43:16 +0200

    Create 08. Centuries to Minutes.js

---
+++ Programming Fundamentals/Data Types and Variables/08. Centuries to Minutes.js
@@ -0,0 +1,17 @@
+function centuriesToMinutes(num) {
+
+    let century=num;
+    const year= 365.2422;
+
+    let years=century*100;
+    let days=Math.trunc(years*year);
+    let hours=days*24;
+    let minutes=hours*60;
+
+    
+
+    console.log(`${century} centuries = ${years} years = ${days} days = ${hours} hours = ${minutes} minutes`)
+
+}
+centuriesToMinutes(1);
+centuriesToMinutes(5);
