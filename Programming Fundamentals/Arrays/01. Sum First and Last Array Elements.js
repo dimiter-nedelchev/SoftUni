9110fc95f1278038bc0f5e9089ec20df653ba519
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2022-12-17 11:11:37 +0200

    Create 01. Sum First and Last Array Elements.js

---
+++ Programming Fundamentals/Arrays/01. Sum First and Last Array Elements.js
@@ -0,0 +1,12 @@
+function sumArrayElements(numbers) {
+
+    let firstElement=numbers[0];
+    let lastElement=numbers[numbers.length-1];
+    let result=firstElement+lastElement;
+
+    console.log(result);
+
+}
+sumArrayElements([20, 30, 40])
+sumArrayElements([10, 17, 22, 33])
+sumArrayElements([11, 58, 69])
