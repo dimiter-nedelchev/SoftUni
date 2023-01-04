2bba5237aa98f2c5b39d5386ebec3a56fa30e0c9
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2023-01-04 07:00:51 +0200

    Create 01. Personal Info.js

---
+++ Programming Fundamentals/Objects and Classes/01. Personal Info.js
@@ -0,0 +1,19 @@
+function personalInfo(firstName,lastName,age) {
+
+let info = {
+    firstName: firstName,
+    lastName: lastName,
+    age: age,
+}
+    return info;
+}
+personalInfo("Peter",
+
+"Pan",
+
+"20");
+personalInfo("George",
+
+"Smith",
+
+"18");
