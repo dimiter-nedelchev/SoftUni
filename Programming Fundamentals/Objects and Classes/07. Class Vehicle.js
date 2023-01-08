64e26d770cbe26ca9d027c21592fd2cb50fe9a3d
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2023-01-08 21:40:58 +0200

    Create 07. Class Vehicle.js

---
+++ Programming Fundamentals/Objects and Classes/07. Class Vehicle.js
@@ -0,0 +1,26 @@
+function vehicle(){
+
+
+    class Vehicle{
+        constructor(type,model,parts,fuel){
+            this.type= type,
+            this.model= model,
+            this.parts= parts,
+            this.parts.quality= parts.engine*parts.power
+            this.fuel= fuel,
+            this.drive= function drive(expenditure){
+                this.fuel-= expenditure;
+            }
+        }
+    }
+   
+    let parts = { engine: 6, power: 100 }
+    
+    let vehicle = new Vehicle('a', 'b', parts, 200);
+    vehicle.drive(100)
+
+    console.log(vehicle.fuel);
+    console.log(vehicle.parts.quality)
+
+}
+vehicle();
