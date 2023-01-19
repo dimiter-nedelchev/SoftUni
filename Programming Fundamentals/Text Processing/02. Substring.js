d8d1b2eab459c5852343014e99345a40696f028d
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2023-01-19 06:43:02 +0200

    Create 02. Substring.js

---
+++ Programming Fundamentals/Text Processing/02. Substring.js
@@ -0,0 +1,8 @@
+function subString(string, start,count){
+
+    const word= string.substring(start, start + count)
+    console.log(word)
+
+}
+subString('ASentence', 1, 8);
+subString('SkipWord', 4, 7);
