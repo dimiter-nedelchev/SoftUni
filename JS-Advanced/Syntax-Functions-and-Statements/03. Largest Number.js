864c401352f3ea2c4bb0a52fc9c416de74887b0f
dimiter-nedelchev <dimitar.nedelchev@outlook.com>
2023-02-17 07:25:22 +0200

    Create 03. Largest Number.js

---
+++ JS-Advanced/Syntax-Functions-and-Statements/03. Largest Number.js
@@ -0,0 +1,6 @@
+function largestNumber(num1, num2, num3){
+
+    let largestNumber= Math.max(num1, num2, num3);
+    console.log(`The largest number is ${largestNumber}.`)
+}
+largestNumber(5, -3, 16)
